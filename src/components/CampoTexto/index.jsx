6e68f4daad3d05742df0b0938678c3c06dbb3578
Biviana Agudelo <biviana_agudelo@hotmail.com>
2024-06-14 14:44:44 -0500

    Aplicando Hook = useState para filtrar las fotos por título en la barra de búsqueda.

--- src/components/CampoTexto/index.jsx
+++ src/components/CampoTexto/index.jsx
@@ -1,44 +1,46 @@
-import { styled } from "styled-components"
-import search from './search.png'
+import { styled } from "styled-components";
+import search from "./search.png";
 
 const ContainerEstilizado = styled.div`
-    position: relative;
-    display: inline-block;
+  position: relative;
+  display: inline-block;
 `;
-
 
 const CampoTextoEstilizado = styled.input`
-    height: 56px;
-    padding: 12px 16px;
-    border-radius: 10px;
-    border: 2px solid;
-    border-color: #C98CF1;
-    background: transparent;
-    box-sizing: border-box;
-    width: 566px;
-    color: #D9D9D9;
-    font-weight: 400;
-    font-size: 20px;
-    line-height: 20px;
-    outline: none;
-`
-
-const IconoLupa = styled.img`
-    position: absolute;
-    top: 10px;
-    right: 10px;
-    width: 38px !important;
-    height: 38px;
+  height: 56px;
+  padding: 12px 16px;
+  border-radius: 10px;
+  border: 2px solid;
+  border-color: #c98cf1;
+  background: transparent;
+  box-sizing: border-box;
+  width: 566px;
+  color: #d9d9d9;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 20px;
+  outline: none;
 `;
 
-const CampoTexto = () => {
-    
+const IconoLupa = styled.img`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  width: 38px !important;
+  height: 38px;
+`;
+
+const CampoTexto = ({ setConsulta }) => {
     return (
         <ContainerEstilizado>
-            <CampoTextoEstilizado type="text" placeholder="¿Qué estás buscando?"/>
+            <CampoTextoEstilizado
+                type="text"
+                placeholder="¿Qué estás buscando?"
+                onChange={(e) => setConsulta(e.target.value)}
+            />
             <IconoLupa src={search} alt="ícono de lupa" />
         </ContainerEstilizado>
-    )
-}
+    );
+};
 
 export default CampoTexto;
--- src/components/Galeria/index.jsx
+++ src/components/Galeria/index.jsx
@@ -1,27 +1,25 @@
-import styled from "styled-components"
-import Titulo from "../Titulo"
-import Populares from "./Populares"
-import Tag from "./Tags"
-import Imagen from "./Imagen"
+import styled from "styled-components";
+import Titulo from "../Titulo";
+import Populares from "./Populares";
+import Tag from "./Tags";
+import Imagen from "./Imagen";
 
 const GaleriaContainer = styled.div`
-display: flex;
-gap: 24px;
-`
+  display: flex;
+  gap: 24px;
+`;
 
 const SeccionFluida = styled.section`
-flex-grow: 1;
-`
+  flex-grow: 1;
+`;
 const ImagenesContainer = styled.section`
-    display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
-    gap: 24px;
-`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  gap: 24px;
+`;
 
-
-const Galeria = ({ fotos = [], alSeleccionarFoto,alAlternarFavorito }) => {
-
+const Galeria = ({ fotos = [], alSeleccionarFoto, alAlternarFavorito, consulta }) => {
     return (
         <>
             <Tag />
@@ -29,19 +27,24 @@ const Galeria = ({ fotos = [], alSeleccionarFoto,alAlternarFavorito }) => {
                 <SeccionFluida>
                     <Titulo>Navegue por la galería</Titulo>
                     <ImagenesContainer>
-                        {fotos.map(foto => <Imagen
-                        alAlternarFavorito= {alAlternarFavorito}
-                        alSolicitarZoom={alSeleccionarFoto}
-                            key={foto.id}
-                            foto={foto} />)
-                        }
+                        {fotos.filter( foto => {
+                            return consulta == '' || foto.titulo.toLowerCase().includes(consulta.toLowerCase())
+                        })
+                        .map((foto) => (
+                            <Imagen
+                                alAlternarFavorito={alAlternarFavorito}
+                                alSolicitarZoom={alSeleccionarFoto}
+                                key={foto.id}
+                                foto={foto}
+                                consulta={consulta}
+                            />
+                        ))}
                     </ImagenesContainer>
                 </SeccionFluida>
                 <Populares />
-
             </GaleriaContainer>
         </>
-    )
-}
+    );
+};
 
-export default Galeria
+export default Galeria;
